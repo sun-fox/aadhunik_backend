26a2a816ee41c7bc272fe5507e0ee157db3ce343
sun-fox <1999sunnykumarsingh@gmail.com>
2019-11-16 14:40:44 +0530

    Addede the REST api for login/ logout and people table CRUD

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/people', function (req, res) {
     }
 	});
 });
-
+// to get all people of specific id
 app.get('/people/:id', function (req, res) {
     var query="select * from people where pid = "+req.params.id+";";
    connection.query(query, function (err, res) {
@@ -43,9 +43,11 @@ app.get('/people/:id', function (req, res) {
       console.log(res[0].name);
 	});
 });
-
-app.post('/people',function(req,res){
+// to add new person to datbase
+app.post('/people/new',function(req,res){
     var query="INSERT INTO `aadhunik`.`people` (`name`, `mobno`, `aadharno`, `emailid`, `password`) VALUES ('"+req.body.name+"','"+req.body.aadharno+"','"+req.body.mobno+"','"+req.body.emailid+"','"+req.body.password+"');";
+    
+    console.log(query);
     connection.query(query, function (err, res) {
         console.log(query);
 	  if (err) throw err;
@@ -56,8 +58,8 @@ app.post('/people',function(req,res){
       }
 	});
 })
-
-app.put('/people/:pid',function(req,res){
+// to edit the credentials of an existing user
+app.put('/people/edit/:pid',function(req,res){
     var query = "Update people SET name='"+req.body.name+"',mobno='"+req.body.mobno+"',aadharno='"+req.body.aadharno+"',emailid='"+req.body.emailid+"',password='"+req.body.password+"' where pid='"+req.params.pid+"';";
     
     console.log(query);
@@ -69,8 +71,8 @@ app.put('/people/:pid',function(req,res){
         }
     })
 })
-
-app.delete("/people/:pid",function(req,res){
+// To delet the credentials of a specific person
+app.delete("/people/delete/:pid",function(req,res){
     var query="DELETE FROM people WHERE pid ="+req.params.pid+";";
     console.log(query);
     connection.query(query,function(err,res){
@@ -81,6 +83,42 @@ app.delete("/people/:pid",function(req,res){
         }
     })
 })
+
+
+// signup route
+app.post('/signup',function(req,res){
+    var query="INSERT INTO `aadhunik`.`people` (`name`, `mobno`, `aadharno`, `emailid`, `password`) VALUES ('"+req.body.name+"','"+req.body.aadharno+"','"+req.body.mobno+"','"+req.body.emailid+"','"+req.body.password+"');";
+    
+    console.log(query);
+    connection.query(query, function (err, res) {
+        console.log(query);
+	  if (err) throw err;
+    //   res.end(JSON.stringify(res));
+    //   console.log(res);
+      if(res.affectedRows>0){
+          console.log("Person Succesfully added!")
+      }
+	});
+})
+
+const logged_in_user="test@gmail.com";
+app.post("/login",function(req,res){
+    var query = "SELECT * FROM `people` where `emailid` = '"+req.body.emailid+"' and `password` = '"+req.body.password+"' ;";
+    console.log(query);
+    connection.query(query, function(err,res){
+        if(err) throw err;
+        console.log(res);
+        // if(!err){
+            // logged_in_user=res[0].emailid;
+            console.log(res[0].emailid);
+        // }
+
+    })
+})
+
+
+
+
 
 //listening at port 3000
 app.listen(3000, "localhost", function () {
